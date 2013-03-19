9dee00f5d7308385ba078d130017a00d1e02d2e0
Cameron Mullen <cameron.mullen@gmail.com>
2013-03-19 14:45:57 -0400

    Added scoring

--- Portal Capital Model/src/com/PortalCapital/NCAA/model/Simulator.java
+++ Portal Capital Model/src/com/PortalCapital/NCAA/model/Simulator.java
@@ -7,6 +7,13 @@ import java.util.Map;
 
 public class Simulator {
 
+	public static final double ROUND_2_SCORE = .01063;
+	public static final double ROUND_3_SCORE = ROUND_2_SCORE + .02;
+	public static final double ROUND_4_SCORE = ROUND_3_SCORE + .0375;
+	public static final double ROUND_5_SCORE = ROUND_4_SCORE + .07;
+	public static final double ROUND_6_SCORE = ROUND_5_SCORE + .13;
+	public static final double ROUND_7_SCORE = ROUND_6_SCORE + .25;
+	
 	
 	Map<Double, List<List<Team>>> storedResults;
 	
@@ -42,20 +49,39 @@ public class Simulator {
 	}
 	
 	
-	private Map<String, List<Integer>> generateTeamResults(List<List<Team>> generalResults) {
-		Map<String, List<Integer>> teamResults = new HashMap<String, List<Integer>>();
+	private Map<String, List<Double>> generateTeamResults(List<List<Team>> generalResults) {
+		Map<String, List<Double>> teamResults = new HashMap<String, List<Double>>();
 		for(List<Team> specificOutcome : generalResults){
 			for(Team t : specificOutcome){
-				List<Integer> specificTeamFinishes = teamResults.get(t.name);
+				List<Double> specificTeamFinishes = teamResults.get(t.name);
 				if(specificTeamFinishes == null){
-					specificTeamFinishes = new ArrayList<Integer>();
+					specificTeamFinishes = new ArrayList<Double>();
 					teamResults.put(t.name, specificTeamFinishes);
 				}
-				specificTeamFinishes.add(t.roundEliminated);
+				specificTeamFinishes.add(getScoreFromFinish(t.roundEliminated));
 			}
 		}
 		
 		return null;
+	}
+	
+	private double getScoreFromFinish(int roundEliminated){
+		switch(roundEliminated){
+		case 2:
+			return ROUND_2_SCORE;
+		case 3:
+			return ROUND_3_SCORE;
+		case 4:
+			return ROUND_4_SCORE;
+		case 5:
+			return ROUND_5_SCORE;
+		case 6:
+			return ROUND_6_SCORE;
+		case 7:
+			return ROUND_7_SCORE;
+		default:
+			return 0;
+		}
 	}
 	
 	
